127fa4907ac0e9187b99e44a557c2456ad4c81b4
wassimjied <wassim.jied@gmail.com>
2019-01-01 21:16:22 +0100

    plugin.js modification
    Add
    Prepost before post image js function
    Success callaback js function
    Failure callback js function

--- README.md
+++ README.md
@@ -5,3 +5,46 @@ plugin for ckeditor allowing single image upload
               |-plugins
                   |-graphicupload
 3- add graphicupload to your toolbar
+4- Declare these configuration parameters
+	-> guCKEUploadUrl   (url to post image to server)
+	   Exemple guCKEUploadUrl: "/uploads/upload.php"     
+	   This parameter is required
+	   
+	-> guCKEPrePostFunc  (javascript function to be executed before file submit. Note inputId is the DOM ID of file input to be sent to server)
+	   Exemple:
+            guCKEPrePostFunc: "function guCKEPrePostFunc(inputId){console.log('pre');}"
+		This parameter is optional and could not be declared
+		
+	-> guCKECallbackSuccessFunc  (javascript function to be executed after submit success. Note inputId is the DOM ID of file input to be sent to server)
+	   Exemple:
+            guCKECallbackSuccessFunc: "function guCKECallbackSuccessFunc(inputId){console.log('success');}"
+		This parameter is optional and could not be declared		
+		
+	-> guCKECallbackFailureFunc  (javascript function to be executed when the submit failed. Note inputId is the DOM ID of file input to be sent to server)
+	   Exemple:
+            guCKECallbackFailureFunc: "function guCKECallbackFailureFunc(inputId){console.log('failure');}"
+		This parameter is optional and could not be declared			
+            
+			
+    ** Note signatures and spelling of previous functions names must be respected
+
+
+Configuration of graphicupload plugin on  symfony 3.x
+config.yml file
+....
+....
+fos_ck_editor:
+    default_config: dashboard
+    input_sync: true        
+    configs:
+        dashboard:
+            toolbar:
+            - [Bold, Italic, Underline, -, graphicupload,-,Cut, Copy, Paste, -, Undo, Redo, -,
+              NumberedList, BulletedList, -, Outdent, Indent, -,
+              Blockquote, -, Link, Unlink, source]
+            extraPlugins: "graphicupload"        
+            guCKEUploadUrl: "/uploads/upload.php"     
+            guCKEPrePostFunc: "function guCKEPrePostFunc(inputId){console.log('pre');}"
+            guCKECallbackSuccessFunc: "function guCKECallbackSuccessFunc(inputId){console.log('success');}"
+            guCKECallbackFailureFunc: "function guCKECallbackFailureFunc(inputId){console.log('failure');}"
+
--- plugin.js
+++ plugin.js
@@ -1,24 +1,45 @@
+/*
+ * graphicupload is a CKEditor plugin allows upload of image to server and insert it in the editor
+ * By  wassim jied 
+ * Blog http://coderspirit.blogspot.com
+ * email wassim.jied@gmail.com
+ * January 1st 2019
+ */
 CKEDITOR.plugins.add( 'graphicupload', {
     icons: 'graphicupload',
     init: function( editor ) {
         var randGUFrmId = '_' + Math.random().toString(36).substr(2, 14);
         var htmlGU="<form enctype='multipart/form-data' method='post' action='/uploads/upload.php' id='frm_"+randGUFrmId+"' style='display:none;'>";
-            htmlGU+="<input type='file' name='upload' id='btn_file_"+randGUFrmId+"'>";
+            htmlGU+="<input type='file' name='upload' id='btn_file_"+randGUFrmId+"' accept='image/png, image/jpeg, image/gif'>";
             htmlGU+="</form>";
         $('body').prepend(htmlGU);
+        /*var guCKEPrePostFunc;
+        var guCKECallbackSuccessFunc;
+        var guCKECallbackFailureFunc;*/
+        if(editor.config.guCKEPrePostFunc !== 'undefined'){                    
+            eval(editor.config.guCKEPrePostFunc);
+        }
+         if(editor.config.guCKECallbackSuccessFunc !== 'undefined'){        
+            eval(editor.config.guCKECallbackSuccessFunc);
+        }    
+        if(editor.config.guCKECallbackFailureFunc !== 'undefined'){        
+            eval(editor.config.guCKECallbackFailureFunc);
+        }
+        
         editor.addCommand('graphicupload', {
-        exec: function (editor) {
-            console.log('fired');
+        exec: function (editor) {            
             $('#btn_file_'+randGUFrmId).click();
             
         }});
         $('#btn_file_'+randGUFrmId).change(function(){
           var file_data = $('#btn_file_'+randGUFrmId).prop('files')[0];
           var form_data = new FormData(); 
-          form_data.append('upload', file_data);
-          
+          form_data.append('upload', file_data);          
+          if (typeof guCKEPrePostFunc == 'function') {
+                guCKEPrePostFunc('btn_file_'+randGUFrmId);
+            }
           $.ajax({
-                url: '/uploads/upload.php', // point to server-side PHP script
+                url: editor.config.guCKEUploadUrl, // point to server-side PHP script
                 dataType: 'text', // what to expect back from the PHP script, if anything
                 cache: false,
                 contentType: false,
@@ -26,13 +47,20 @@ CKEDITOR.plugins.add( 'graphicupload', {
                 data: form_data,
                 type: 'post',
                 success: function(data){
-                    // get server responce here
-                    alert(data);
+                    if (typeof guCKECallbackSuccessFunc == 'function') {                    
+                        guCKECallbackSuccessFunc('btn_file_'+randGUFrmId);
+                    }
+
                     imgDomElem = CKEDITOR.dom.element.createFromHtml("<img src='"+data+"'/>")
                     editor.insertElement(imgDomElem);                    
+                },
+                failure:function(){
+                   if (typeof guCKECallbackFailureFunc == 'function') {
+                        guCKECallbackFailureFunc('btn_file_'+randGUFrmId);
+                    }                    
                 }
             });
-            // clear file field
+            document.getElementById('btn_file_'+randGUFrmId).value = "";
        })
 
         editor.ui.addButton( 'graphicupload', {
